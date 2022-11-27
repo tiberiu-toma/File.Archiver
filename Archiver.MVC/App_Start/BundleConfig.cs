fca839005421d1af395c36decadde8f3c5b9cce2
Tiberiu Toma <tomageorgetiberiu@gmail.com>
2022-11-27 23:13:39 +0200

    Updated some packages

--- Archiver.MVC/App_Start/BundleConfig.cs
+++ Archiver.MVC/App_Start/BundleConfig.cs
@@ -18,7 +18,7 @@ namespace Archiver.MVC
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
 
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+            bundles.Add(new Bundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
